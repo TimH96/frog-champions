f768ee99c09fd1a3e74f96ccfafa266fc562d902
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-04-25 23:58:04 +0200

    boilerplate for filterfunction

---
+++ src/ts/speedruncom/helper.ts
@@ -0,0 +1,12 @@
+import SpeedrunId from "./types/SpeedrunId";
+import SpeedrunRun from "./types/SpeedrunRun";
+
+type subcatFilterFunction = (run: SpeedrunRun) => boolean;
+
+const newSubcatFilter = (subcatId: SpeedrunId, valueId: SpeedrunId): subcatFilterFunction => {
+    return (run: SpeedrunRun) => {
+        return run.values![subcatId] == valueId
+    }
+}
+
+export { newSubcatFilter }
