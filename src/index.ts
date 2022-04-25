b43095de2c575696e0455fb7d39797d5e0f228b7
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-04-25 21:47:22 +0200

    testing boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,18 @@
-var possibleEvents = new Set(["input", "onpropertychange", "keyup", "change", "paste"]);
+import SpeedrunApiResponse from "./ts/speedruncom/types/SpeedrunApiResponse";
+import SpeedrunId from "./ts/speedruncom/types/SpeedrunId";
+import SpeedrunLevel from "./ts/speedruncom/types/SpeedrunLevel";
 
-window.onload = () => {
-    var ticksInput = document.getElementById("ticks") as HTMLInputElement;
-    possibleEvents.forEach((eventName: string) => {
-        ticksInput.addEventListener(eventName, (ev: Event) => {
-            var inputElement = ev.target as HTMLInputElement;
-            var handler = new TickInputHandler();
-            handler.showResult(inputElement);
-        })
-    });
-};
+export default interface Player {
+    name: string
+    id: SpeedrunId
+}
 
-class TickInputHandler {
-    // Ticks value for date 01-01-1970
-    static epochTicks: number = 621355968000000000;
-    static ticksPerMillisecond: number = 10000;
-    // http://ecma-international.org/ecma-262/5.1/#sec-15.9.1.1
-    static maxDateMilliseconds: number = 8640000000000000;
-
-    public showResult(inputElement: HTMLInputElement) {
-        // Get value from the input and try to convert it to type Number
-        var valueStr = inputElement.value;
-        var ticks = Number(valueStr);
-
-        var dateString = String();
-        // If we were not able to parse input as a Number - show empty DateTimeString
-        if (isNaN(ticks)) {
-            dateString = "____-__-__T__:__:__.____Z";
-        }
-
-        // convert the ticks into something typescript understands
-        var ticksSinceEpoch = ticks - TickInputHandler.epochTicks;
-        var millisecondsSinceEpoch = ticksSinceEpoch / TickInputHandler.ticksPerMillisecond;
-        // If the value of the input is more than max value - show special DateTime string for this case
-        if (millisecondsSinceEpoch > TickInputHandler.maxDateMilliseconds) {
-            dateString = "9999-99-99T99:99:99:9999Z";
-        }
-
-        // output the result in something the human understands
-        var date = new Date(millisecondsSinceEpoch);
-        return date.toISOString();
-        var dateTimeOutput = document.getElementById("datetime");
-        dateTimeOutput.innerHTML = dateString;
+async function main() {
+    const levelsRes: SpeedrunApiResponse<SpeedrunLevel[]> = await (await fetch("https://www.speedrun.com/api/v1/games/o1y9j9v6/levels")).json()
+    for (const level of levelsRes.data) {
+        const categoriesRes: SpeedrunApiResponse<any> = await (await fetch(level.links[2].uri)).json()
+        console.log(categoriesRes)
     }
 }
+
+main()
