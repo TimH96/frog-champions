3bf71275269fb0732804d3d8494a2f9bfc4f1d77
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-04-26 20:58:22 +0200

    include id in player

--- src/lib/models/Player.ts
+++ src/lib/models/Player.ts
@@ -1,12 +1,15 @@
+import SpeedrunId from "../speedruncom/response_types/SpeedrunId";
 import RankedRun from "./RankedRun";
 import RankingGrid from "./RankingGrid";
 
 export default class Player {
+    id: SpeedrunId;
     timesPage: RankingGrid;
     totalPoints: number;
     categoryPoints: number[];
 
-    constructor(gridDimensions: number[]) {
+    constructor(id: SpeedrunId, gridDimensions: number[]) {
+        this.id = id;
         this.timesPage = gridDimensions.map(colSize => new Array<RankedRun>(colSize))
         this.totalPoints = 0
         this.categoryPoints = gridDimensions.map(_ => 0);
