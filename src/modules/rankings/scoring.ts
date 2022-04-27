8d1e2d95a1eca5bb4193d47d4962ca2e3649fc50
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-04-27 23:43:08 +0200

    rename r to run

--- src/modules/rankings/scoring.ts
+++ src/modules/rankings/scoring.ts
@@ -2,10 +2,10 @@ import SpeedrunRankedRun from '../speedruncom/models/SpeedrunRankedRun'
 
 type scoringFunction = (r: SpeedrunRankedRun) => number
 
-const eliteScoring = (r: SpeedrunRankedRun): number => {
-  if (r.place === 1) { return 100 }
-  if (r.place === 2) { return 97 }
-  return Math.max(0, 98 - r.place)
+const eliteScoring = (run: SpeedrunRankedRun): number => {
+  if (run.place === 1) { return 100 }
+  if (run.place === 2) { return 97 }
+  return Math.max(0, 98 - run.place)
 }
 
 export { scoringFunction, eliteScoring }
