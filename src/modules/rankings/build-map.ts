08cb819822f9dc9ed92dd45db56bbe064d9f70ba
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-04-27 20:53:26 +0200

    only register run in map build

--- src/modules/rankings/build-map.ts
+++ src/modules/rankings/build-map.ts
@@ -6,26 +6,18 @@ const buildPlayerMap = async (grid: LevelGrid): Promise<Map<SpeedrunId, Player>>
   const pMap = new Map<SpeedrunId, Player>()
   const gridDimensions = grid.map((col) => col.length)
 
-  grid.forEach((levelColumn, i) => {
-    levelColumn.forEach((board, j) => {
-      board.runs.forEach((run, k) => {
-        const r = run.run
-        const p = r.players[0]
+  grid.forEach((levelColumn, i) =>
+    levelColumn.forEach((board, j) =>
+      board.runs.forEach((r) => {
+        const p = r.run.players[0]
 
         pMap.has(p.id) || pMap.set(p.id, new Player(p.id, gridDimensions))
 
         const pl = pMap.get(p.id)!
-        const points = Math.max(100 - k, 0)
-        pl.timesPage[i][j] = {
-          run: r,
-          place: k + 1,
-          points
-        }
-        pl.totalPoints += points
-        pl.categoryPoints[i] = pl.categoryPoints[i] + points
+        pl.registerRun(r, i, j)
       })
-    })
-  })
+    )
+  )
 
   return pMap
 }
