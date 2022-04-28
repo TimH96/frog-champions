271eac8101f8e9a201e059aadc5fe314029cc472
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-04-28 21:06:56 +0200

    center loader

--- src/app/ui/components/loader.ts
+++ src/app/ui/components/loader.ts
@@ -1,7 +1,7 @@
 import htmlToElement from '../html-helper'
 
 const getLoader = () => {
-  return htmlToElement('<div class="loader">Loading...</div>')
+  return htmlToElement('<div class="loader center">Loading...</div>')
 }
 
 export default getLoader
