c633b2a0a681302fe0b63b8f293a9195aed42515
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-04-28 21:48:02 +0200

    Updates

--- src.77de5100.js
+++ src.77de5100.js
@@ -1939,7 +1939,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55462" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54142" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
