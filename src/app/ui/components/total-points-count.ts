5c21e51631582675a2d6fd9b30a80e267c1379fe
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-04-30 12:20:33 +0200

    add some player page styles

--- src/app/ui/components/total-points-count.ts
+++ src/app/ui/components/total-points-count.ts
@@ -3,6 +3,7 @@ import PlayerState from '../../states/PlayerState'
 const getTotalPointsCount = (s: PlayerState) => {
   const x = document.createElement('span')
   x.classList.add('center')
+  x.classList.add('total-points')
   x.innerHTML = `Total: ${s.player!.totalPoints}`
   return x
 }
