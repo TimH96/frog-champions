8389bb251dbb43f33bb540eaa9fdbb7265571767
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-05-01 16:35:53 +0200

    fix scoring to use 0 as min score

--- src/modules/rankings/scoring.ts
+++ src/modules/rankings/scoring.ts
@@ -12,7 +12,7 @@ const getPercentScoreFn = (wrTime: number) => {
   return (r: SpeedrunRankedRun) => {
     const t = r.run.times.primary_t
 
-    return Math.round((1 - Math.log((Math.E - 1) * (t / wrTime - 1) + 1)) * 1000)
+    return Math.max(0, Math.round((1 - Math.log((Math.E - 1) * (t / wrTime - 1) + 1)) * 1000))
   }
 }
 
